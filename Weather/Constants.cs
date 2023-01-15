ed43bd05bc9a57c945e6669cd0bfbd4937f800cd
bapbo <bapbo@VANILLACOKE>
2023-01-15 23:38:26 +1300

    Add project files.

---
+++ Weather/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "aec6e4dc7c53f401139a31cbf4a48d5d";
+        public static string OpenWeatherMapImageEndpoint = "http://openweathermap.org/img/wn/";
+    }
+}
---
+++ Weather/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Input;
+
+namespace Weather;
+
+public partial class MainPage : ContentPage
+{
+	RestService _restService;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		_restService = new RestService();
+	}
+
+    private async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+    {
+		if(!string.IsNullOrWhiteSpace(_cityEntry.Text)) 
+		{
+			WeatherData weatherData = await 
+				_restService.
+				GetWeatherData(GenerateRequestURL(Constants.OpenWeatherMapEndpoint));
+
+            weatherData.Weather[0].Icon = Constants.OpenWeatherMapImageEndpoint + weatherData.Weather[0].Icon + ".png";
+			weatherData.Dt = weatherData.Dt + weatherData.Timezone;
+			weatherData.Sys.Sunrise = weatherData.Sys.Sunrise + weatherData.Timezone;
+			weatherData.Sys.Sunset = weatherData.Sys.Sunset + weatherData.Timezone;
+
+            BindingContext = weatherData;
+
+        }
+    }
+
+	string GenerateRequestURL(string endPoint)	
+	{
+		string requestUri = endPoint;
+		requestUri += $"?q={_cityEntry.Text}";
+		requestUri += "&units=metric";
+		requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+		return requestUri;
+	}
+}
+
